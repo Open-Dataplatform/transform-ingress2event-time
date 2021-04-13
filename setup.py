81156c9d2bae56e875ea742476dee6e8537f8a96
Araz B. Makoo <araz100@gmail.com>
2021-04-13 08:52:06 +0200

    implemented the transformation

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="osiris-transform-ingress2event-time",
+    packages=find_packages()
+)
---
+++ transform_ingress2event_time/transform.py
@@ -0,0 +1,73 @@
+"""
+Transforms data structured in the filesystem according to the ingress time to the event time for the data.
+The data gets accumulated daily.
+"""
+import argparse
+import sys
+from datetime import datetime
+
+from osiris.pipeline_timeseries import PipelineTimeSeries
+from .configuration import Configuration
+
+configuration = Configuration(__file__)
+config = configuration.get_config()
+logger = configuration.get_logger()
+
+
+def __init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Transform from ingress time to event time accumulated daily')
+
+    parser.add_argument('--ingress_time', type=str, default=None, help='the ingress time to start the ingress from.')
+
+    return parser
+
+
+def __get_pipeline() -> PipelineTimeSeries:
+    account_url = config['Azure Storage']['account_url']
+    filesystem_name = config['Azure Storage']['filesystem_name']
+
+    tenant_id = config['Authorization']['tenant_id']
+    client_id = config['Authorization']['client_id']
+    client_secret = config['Authorization']['client_secret']
+
+    source = config['Datasets']['source']
+    destination = config['Datasets']['destination']
+    date_key_name = config['Datasets']['date_key_name']
+    date_format = config['Datasets']['date_format']
+
+    try:
+        return PipelineTimeSeries(storage_account_url=account_url,
+                                  filesystem_name=filesystem_name,
+                                  tenant_id=tenant_id,
+                                  client_id=client_id,
+                                  client_secret=client_secret,
+                                  source_dataset_guid=source,
+                                  destination_dataset_guid=destination,
+                                  date_format=date_format,
+                                  date_key_name=date_key_name)
+    except Exception as error:  # noqa pylint: disable=broad-except
+        logger.error('Error occurred while initializing pipeline: %s', error)
+        sys.exit(-1)
+
+
+def main():
+    """
+    The main function which runs the transformation.
+    """
+    argparser = __init_argparse()
+    args = argparser.parse_args()
+    pipeline = __get_pipeline()
+
+    try:
+        if args.ingress_time:
+            ingress_time = datetime.strptime(args.ingress_time, '%Y-%m-%dT%H')
+            pipeline.transform_ingest_time_to_event_time_daily(ingress_time)
+        else:
+            pipeline.transform_ingest_time_to_event_time_daily()
+    except Exception as error:  # noqa pylint: disable=broad-except
+        logger.error('Error occurred while running pipeline: %s', error)
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    main()
