34724dfc27f96d6751f25018e47f277060c0ac16
Mads Hovgård Jakobsen <ragerin@gmail.com>
2021-11-02 15:21:01 +0100

    add github actions

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+# TODO: Implement unittests
+class PlaceholderTest(unittest.TestCase):
+    def test_placerholder(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
